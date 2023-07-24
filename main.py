487e33a22d4d63fc560ecb1705d267d0b81ee41a
guilherme <jsoares.2019@hotmail.com>
2023-07-24 15:45:46 -0300

    adotando formatação do python 3.6

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print(minhaConta.getSaldo())
 back="N"
 while True:
     print("===MENU===")
-    print("acessando a conta de {}".format(minhaConta.NomeTitular))
+    print(f"acessando a conta de {minhaConta.NomeTitular}")
     print("\033[1;37m[1]\033[m-Depositar\n\033[1;37m[2]\033[m-Sacar\n\033[1;37m[3]\033[m-Verificar o Saldo\n\033[1;37m[4]\033[m-Sair")
     option=input("Qual opção deseja executar?\n").strip()
     if option == '4':
@@ -27,7 +27,7 @@ while True:
             if deposito.isdigit():
                 deposito = float(deposito)
                 minhaConta.depositar(deposito)
-                print("Foi depositado R${:.2f} na sua conta".format(deposito))
+                print(f"Foi depositado R${deposito:.2f} na sua conta")
                 break
             else:
                 print("\033[1;31mOpção inválida. Por favor, digite apenas números!!\033[m")
@@ -37,12 +37,12 @@ while True:
             if sacar.isdigit():
                 sacar=float(sacar)
                 minhaConta.sacar(sacar)
-                print("Você sacou {:.2f} do seu saldo".format(sacar))
+                print(f"Você sacou {sacar:.2f} do seu saldo")
                 break
             else:
                 print("\033[1;31mOpção inválida. Por favor, digite apenas números!!\033[m")
     elif option == '3':
-        print("Você tem R${:.2f}".format(minhaConta.getSaldo()))
+        print(f"Você tem R${minhaConta.getSaldo():.2f}")
     else:
         print("\033[1;31mOpção inválida,Por favor digite apenas oque esta sendo mostrado!!\033[m")
     while True:
